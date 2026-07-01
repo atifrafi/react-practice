d61bade389008f0a1bf65b0c6008294fe16f4350
atif rafique <apt848696@gmail.com>
2026-07-01 18:22:29 +0500

    create component

---
+++ src/components/User.jsx
@@ -0,0 +1,22 @@
+const User = () =>{
+    return (
+        <div>
+            <h1>Users</h1>
+            <Info/>
+        </div>
+    )
+}
+
+export default User;
+
+const Info = () => {
+    return (
+        <div>
+            <h2>User Name</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    )
+}
+
+
+// note ek hi page mein multiple component create karna
