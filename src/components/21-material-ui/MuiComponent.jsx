6ae841df3b6c56ad40695dd5ffa537dfe99da083
atif rafique <apt848696@gmail.com>
2026-07-11 12:08:10 +0500

    material ui

---
+++ src/components/21-material-ui/MuiComponent.jsx
@@ -0,0 +1,13 @@
+import {Button} from '@mui/material'
+
+const MuiComponent =()=>{
+    return(
+        <>
+        <Button variant='contained' color='success'>
+            click here
+        </Button>
+        </>
+    )
+}
+
+export default MuiComponent;
